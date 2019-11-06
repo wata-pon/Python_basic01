d62714fef215971cdb8bfd9d2fb03c136c9aba7d
wata-pom <w.kumagai1224@gmail.com>
2019-11-06 13:52:53 +0900

    BMI計算アプリの実装
    身長の値を入力し、出力する

--- A-11.py
+++ A-11.py
@@ -3,20 +3,13 @@
 参考となるWebアプリ https://keisan.casio.jp/exec/system/1161228732
 小数点第2位まで表示する
 
-とりあえず
-height 168cm
-weight 62kg
 """
 
 
 class BMI:
-    def __init__(self, weight, height):
-        self.height = height
-        self.weight = weight
-
-    def calculate_bmi(self):
-        return f'{self.weight / (self.height / 100) ** 2 :.2f}'
+    pass
 
 
-personal_bmi = BMI(weight=62, height=168)
-print(personal_bmi.calculate_bmi())
+bmi = BMI()
+bmi.height = int(input("身長、入力して-:")) / 100
+print(bmi.height)
